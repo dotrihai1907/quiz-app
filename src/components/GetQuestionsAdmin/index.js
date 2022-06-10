5fa55e31c3936c2990117e381385611a298f32ef
Đỗ trí Hải <dotrihai1999@gmail.com>
2022-06-11 00:42:23 +0700

    main

--- src/components/GetQuestionsAdmin/index.js
+++ src/components/GetQuestionsAdmin/index.js
@@ -237,7 +237,6 @@ function GetQuestionsAdmin() {
 
   const EditableContext = createContext(null);
   const EditableRow = ({ index, ...props }) => {
-    const [form] = Form.useForm();
     return (
       <Form form={form} component={false}>
         <EditableContext.Provider value={form}>
