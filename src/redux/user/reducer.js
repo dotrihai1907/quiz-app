a8f4a8d245248b9d5437403baf9c93c4e44a84a1
Đỗ trí Hải <dotrihai1999@gmail.com>
2022-06-14 16:03:52 +0700

    main

--- src/redux/user/reducer.js
+++ src/redux/user/reducer.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const userSlice = createSlice({
   name: "user",
   initialState: {
-    users: null,
+    users: undefined,
   },
   reducers: {
     getUsersSuccess: (state, action) => {
